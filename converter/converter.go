a6e400e0f3c2f02530c5ea630ad2d89d2a266e73
rujche <rujche@microsoft.com>
2025-02-14 10:49:50 +0800

    Add converter.go.

---
+++ converter/converter.go
@@ -0,0 +1,12 @@
+package converter
+
+import (
+	"ajpa/analyzer"
+
+	"github.com/azure/azure-dev/cli/azd/pkg/azdext"
+)
+
+func ProjectAnalysisResultToAzdProjectConfig(result analyzer.ProjectAnalysisResult) azdext.ProjectConfig {
+	// todo implement this.
+	return azdext.ProjectConfig{}
+}
